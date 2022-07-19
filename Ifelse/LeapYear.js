8a4be87303ecaf82b8fa08ef2c2bd02832119f99
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-20 00:04:36 +0530

    Day 38 Js Pratice Problem.

---
+++ Ifelse/LeapYear.js
@@ -0,0 +1,7 @@
+/** Check leap year */
+const prompts = require("prompt-sync")();
+let year = parseInt(prompts("Enter the year : "));
+if (year % 4 == 0 && (year % 100 != 0 | year % 400 == 0))
+    console.log(year + " is a Leap year");
+else 
+    console.log(year + " is not a Leap year");  
